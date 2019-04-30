139671ed3d3b16d69693f58a1ca50fe4f73cdf27
adaltezze <adaltezze@gmail.com>
2019-04-30 09:20:03 +0500

    update

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,9 +3,17 @@ import React from 'react';
 import TodoListItem from './todo-list-item.js';
 
 function TodoList({ todos }) {
+    const elements = todos.map(({ label, important }) =>
+        <li key={label}>
+            <TodoListItem
+                label={label}
+                important={important} />
+        </li>
+    );
+
     return (
         <ul>
-            {todos.map(({ label, important }) => <li key={label} ><TodoListItem label={label} important={important} /></li>)}
+            { elements }
         </ul>
     );
 }
