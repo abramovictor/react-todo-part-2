189474d5f27f84d811db387e6c01fbfbbc20085b
adaltezze <adaltezze@gmail.com>
2019-05-07 15:00:59 +0500

    update

--- src/components/todo-list/todo-list.jsx
+++ src/components/todo-list/todo-list.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import TodoListItem from '../todo-list-item';
+import CSSTransitionGroup from 'react-addons-css-transition-group'
 
 import './todo-list.css';
 
 export default function TodoList({ todos, onDeleteTodo, onToggleImportantTodo, onToggleDoneTodo, onLabelChange }) {
     return (
-        <div className="card-body">
-            <ul className="list-group">
-                {todos.length ?
-                    todos.map(({ id, ...todoProps }) =>
+        <div className="todo-list card-body">
+            <CSSTransitionGroup
+                component="ul"
+                transitionName="todo-list--slide"
+                transitionEnterTimeout={300}
+                transitionLeaveTimeout={300}
+                transitionAppear={true}
+                transitionAppearTimeout={1000}
+                className="list-group">
+                {todos.length
+                    ? todos.map(({ id, ...todoProps }) =>
                         <li
-                            className="todo-list border border-light d-flex align-items-center list-group-item p-0 my-1 shadow"
+                            className="border border-light d-flex align-items-center list-group-item p-0 my-1 shadow"
                             key={id}>
                             <TodoListItem
                                 id={id}
@@ -20,10 +28,10 @@ export default function TodoList({ todos, onDeleteTodo, onToggleImportantTodo, o
                                 onLabelChange={onLabelChange}
                                 {...todoProps} />
                         </li>
-                    ) :
-                    <li className="list-group-item text-center text-muted">No to-dos</li>
+                    )
+                    : <li className="list-group-item text-center text-muted">No to-dos</li>
                 }
-            </ul>
+            </CSSTransitionGroup>
         </div>
     );
 }
